8fb1b2e1b7e995872de0eb4ba94d6365a6d419c7
Florian Hauschild <florian.hauschild@hensoldt.net>
2022-06-07 16:27:09 +0200

    [NEW] SEOS-3369 Start developing custome example from virtio_net example
    * Changed virtio_net to instantiate two vms
    * Create own initrd for each vm
    * Currently use Serial peripheral output of one vm
    * VM1 is not yet connected on the networking side

---
+++ apps/Arm/vm_virtio_net_2_VMs/settings.cmake
@@ -0,0 +1,24 @@
+#
+# Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+set(supported "exynos5422;tx2;qemu-arm-virt")
+if(NOT "${PLATFORM}" IN_LIST supported)
+    message(FATAL_ERROR "PLATFORM: ${PLATFORM} not supported.
+         Supported: ${supported}")
+endif()
+set(LibUSB OFF CACHE BOOL "" FORCE)
+set(VmPCISupport ON CACHE BOOL "" FORCE)
+set(VmVirtioNetVirtqueue ON CACHE BOOL "" FORCE)
+set(VmInitRdFile ON CACHE BOOL "" FORCE)
+#set(VmDtbFile ON CACHE BOOL "provide dtb" FORCE)
+if(${PLATFORM} STREQUAL "qemu-arm-virt")
+    # force cpu
+    set(QEMU_MEMORY "3072")
+    set(KernelArmCPU cortex-a53 CACHE STRING "" FORCE)
+endif()
+
+# use ZF_LOG_DEBUG (2) by default, apps and components can change this
+set(LibUtilsDefaultZfLogLevel 2 CACHE STRING "")
